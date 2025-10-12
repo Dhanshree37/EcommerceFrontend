56889ce37450f6da846ef355bb9ff4f8df779d64
Dhananjay <dvp171098@gmail.com>
2025-10-12 17:48:42 +0530

    added new version changes config for latest angular

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,18 @@
 import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app/app.module';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http';
+import { provideRouter } from '@angular/router';
+import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
+import { routes } from './app/app-routing.module'; // update for standalone
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideHttpClient(),
+    provideRouter(routes)  // your routes
+  ]
+}).catch(err => console.error(err));
