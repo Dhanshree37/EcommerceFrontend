4313b914ada21b714b70b1c71ba53e0151e8fae5
Dhanshree <dhanshreevpit121@siesgst.ac.in>
2025-11-17 16:38:53 +0530

    Register page UI

--- src/app/modules/auth/register/register.ts
+++ src/app/modules/auth/register/register.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MATERIAL_IMPORTS } from 'src/app/shared/material/material.imports';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [
+    MATERIAL_IMPORTS,
+    ReactiveFormsModule,
+    FormsModule,
+  ],
   templateUrl: './register.html',
   styleUrl: './register.scss'
 })
